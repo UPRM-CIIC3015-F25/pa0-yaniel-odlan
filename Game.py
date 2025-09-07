af7ad215229bd335b45285edc6e8b8d3068c0feb
Yaniel2007 <yaniel.soto1@upr.edu>
2025-09-07 13:38:32 -0400

    Conflict Merge

--- Game.py
+++ Game.py
@@ -16,7 +16,7 @@ def ball_movement():
 
     # Start the ball movement when the game begins
     # TODO Task 5 Create a Merge Conflict
-    speed = 5
+    speed = 4
     if start:
         ball_speed_x = speed * random.choice((1, -1))  # Randomize initial horizontal direction
         ball_speed_y = speed * random.choice((1, -1))  # Randomize initial vertical direction
@@ -101,7 +101,7 @@ start = False  # Indicates if the game has started
 while True:
     # Event handling
     # TODO Task 4: Add your name
-    name = "John Doe"
+    name = "Yaniel E. Soto Ruiz"
     for event in pygame.event.get():
         if event.type == pygame.QUIT:  # Quit the game
             pygame.quit()
